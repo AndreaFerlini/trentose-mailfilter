f72a2acde669309cf21bcdb507e21574d66c42c4
Andrea Ferlini <AndreaFerlini@MacBook-Pro-di-Ferla.local>
2015-12-05 16:16:01 +0100

    starting test cases

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /* your code should go here */
-
+var temp= '<li id="ID">MSG</li>'
 
 // You can modify this object, add functions that you need
 var MailModel = {
@@ -16,8 +16,17 @@ var MailModel = {
     * Filters out messages in the "database" that match the spam rules.
     * @return an array of messages, excluding those that match the filter rules.
     */
-    filter : function(){
-      return [];
+    filter : function(){ //aggiungere elementi ad un array nuovo con filteredMsg[filteredMsg.length]=elm 
+      var filteredMsg = this.messages;
+      for (var i=0; i<filteredMsg.length; i++){
+          for (var j=0; j<this.rules.length; j++){
+              if (filteredMsg[i].indexOf(this.rules[j])>=0){
+                  filteredMsg.splice(i, 1); //non funziona correttamente
+              }
+          }
+      }
+        console.log(filteredMsg);
+        return filteredMsg;
     }
 
   
@@ -28,12 +37,53 @@ var MailModel = {
 // MailModel.filter() 
 //  -> ["carlo@gmail.com", "trentose2@googlegroups.com"]
 
+var MailController = {
+    start : function(){
+        MailModel.init();
+        MailView.LoadingMsg();
+    },
+    GetMsgLen : function(){
+        return MailModel.messages.length;
+    },
+    GetMsgAddr : function (i){
+        return MailModel.messages[i];
+    },
+    GetFilteredLen : function (){
+        var Msg = MailModel.filter();
+        return Msg.length;
+    },
+    GetFilteredAddr : function (i){
+        var Msg = MailModel.filter();
+        return Msg[i];
+    }
+};
 
-// We suggest to use js patters. 
-// you can add here your views and controllers if you decide to do so.
-
-
+var MailView = {
+    LoadingMsg : function(){
+        $(".result").html("");
+        var temp1;
+        for (var i=0; i<MailController.GetMsgLen(); i++){
+            temp1 = temp.replace("ID", i).replace("MSG", MailController.GetMsgAddr(i))
+            $(".result").append(temp1);
+        }
+        this.filterClick();
+    },
+    filterClick : function(){
+        $(".btn-filter").click(function(){
+            MailView.LoadFilteredMsg();
+        })
+    },
+    
+    LoadFilteredMsg : function(){
+        $(".result").html("");
+        var temp1;
+        for (var i=0; i<MailController.GetFilteredLen(); i++){
+            temp1 = temp.replace("ID", i).replace("MSG", MailController.GetFilteredAddr(i));
+            $(".result").append(temp1);
+        }
+    }
+};
 
 $(document).ready(function(){
-
+    MailController.start();
 });
--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -1,5 +1,49 @@
 describe("MailModel", function() {
+  it("init should work properly", function(){
+    MailModel.init();
+    var rules = MailModel.rules;
+    var messages = MailModel.messages;
+      
+    expect(regole).toBe(["spam.com", "compratutto.it"]);
+    expect(messaggi).toBe(["news@spam.com", "carlo@gmail.com", "jessy@compratutto.it", "trentose2@googlegroups.com"]);
+  });
+    
+  /*it("filter should work properly", function(){
+    
+  });
+*/ 
+});
+
+/*
+describe("MailController", function() {
   it("should..", function(){
+  
+  });
+  it("should..", function(){
+  
+  });
+  it("should..", function(){
+  
+  });
+  it("should..", function(){
+  
+  });
+  it("should..", function(){
+  
   });
  
 });
+
+describe("MailView", function() {
+  it("should..", function(){
+  
+  });
+  it("should..", function(){
+  
+  });
+  it("should..", function(){
+  
+  });
+ 
+});
+*/
